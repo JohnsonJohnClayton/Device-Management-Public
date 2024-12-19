73e1cfa16689049a40a59ca86220fbee72fbb280
John Johnson <john@zenguard.io>
2024-12-19 17:07:58 -0600

    Update Windows UPdate logic to run concurrently

--- Device Provisioning/Setup-SCSDevice.ps1
+++ Device Provisioning/Setup-SCSDevice.ps1
@@ -66,38 +66,41 @@ $ProgressPreference = $ProgressPreference_bk
 ###################################
 ########## Windows Update #########
 ###################################
-Write-Host "Running Windows Updates..."
+Write-Host "Running Windows Updates in a concurrent process..."
+$job = Start-Job -ScriptBlock {
+    # Setup Windows Update
+    # Check if NuGet package provider is available
+    $nuget = Get-PackageProvider 'NuGet' -ListAvailable -ErrorAction SilentlyContinue
 
-# Setup Windows Update
-# Check if NuGet package provider is available
-$nuget = Get-PackageProvider 'NuGet' -ListAvailable -ErrorAction SilentlyContinue
+    # Install NuGet package provider if not found
+    if ($null -eq $nuget) {
+        Install-PackageProvider -Name NuGet -Confirm:$false -Force
+    }
 
-# Install NuGet package provider if not found
-if ($null -eq $nuget) {
-    Install-PackageProvider -Name NuGet -Confirm:$false -Force
+    # Check if the PSWindowsUpdate module is available
+    $module = Get-Module 'PSWindowsUpdate' -ListAvailable
+
+    # Install PSWindowsUpdate module if not found
+    if ($null -eq $module) {
+        Install-Module PSWindowsUpdate -Confirm:$false -Force
+    }
+
+    # Retrieve available Windows updates
+    $updates = Get-WindowsUpdate 
+
+    # Install Windows updates if any are available
+    if ($null -ne $updates) {
+        Install-WindowsUpdate -AcceptAll -Install -IgnoreReboot | 
+        Select-Object KB, Result, Title, Size  # Select specific properties to display
+    }
+
+    # Check if a reboot is required after updates are installed
+    $status = Get-WURebootStatus -Silent
+
+    Write-Host "`nWindows Updates Complete`n" -ForegroundColor Green
 }
+$result = $job | Wait-Job | Receive-Job
 
-# Check if the PSWindowsUpdate module is available
-$module = Get-Module 'PSWindowsUpdate' -ListAvailable
-
-# Install PSWindowsUpdate module if not found
-if ($null -eq $module) {
-    Install-Module PSWindowsUpdate -Confirm:$false -Force
-}
-
-# Retrieve available Windows updates
-$updates = Get-WindowsUpdate 
-
-# Install Windows updates if any are available
-if ($null -ne $updates) {
-    Install-WindowsUpdate -AcceptAll -Install -IgnoreReboot | 
-    Select-Object KB, Result, Title, Size  # Select specific properties to display
-}
-
-# Check if a reboot is required after updates are installed
-$status = Get-WURebootStatus -Silent
-
-Write-Host "`nWindows Updates Complete`n" -ForegroundColor Green
 
 ###################################
 ############# Debloat #############
